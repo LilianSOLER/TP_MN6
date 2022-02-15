e61b1ef1bceb1f86aff49ad4e299c5de13b7501a
Lilian SOLER <soler.lilian64@gmail.com>
2022-02-15 17:48:13 +0100

    Correction struc poly_creux

--- poly/poly.h
+++ poly/poly.h
@@ -35,27 +35,27 @@ p_polyf_t puissance_polynome (p_polyf_t p, int n) ;
 
 p_polyf_t composition_polynome (p_polyf_t p, p_polyf_t q) ;
 
-// //POLYNOME CREUX
-// typedef struct polyf_creux_t {
-//   int degre ;
-//   float coeff;
-//   polyf_creux_t *next;
-// } polyf_creux_t, *p_polyf_creux_t;
+//POLYNOME CREUX
+typedef struct poly_creux{
+  int degre ;
+  float coeff;
+  struct poly_creux *next;
+} polyf_creux_t, *p_polyf_creux_t;
 
-// p_polyf_creux_t creer_polynome_creux (int degre) ;
-// void detruire_polynome_creux (p_polyf_creux_t p);
-// void init_polynome_creux (p_polyf_creux_t p, float x);
+p_polyf_creux_t creer_polynome_creux (int degre) ;
+void detruire_polynome_creux (p_polyf_creux_t p);
+void init_polynome_creux (p_polyf_creux_t p, float x);
 
-// p_polyf_creux_t lire_polynome_creux_float (char *nom_fichier);
-// void ecrire_polynome_creux_float (p_polyf_creux_t p);
+p_polyf_creux_t lire_polynome_creux_float (char *nom_fichier);
+void ecrire_polynome_creux_float (p_polyf_creux_t p);
 
-// int egalite_polynome_creux (p_polyf_creux_t p1, p_polyf_creux_t p2);
+int egalite_polynome_creux (p_polyf_creux_t p1, p_polyf_creux_t p2);
 
-// p_polyf_creux_t addition_polynome_creux (p_polyf_creux_t p1, p_polyf_creux_t p2);
-// p_polyf_creux_t multiplication_polynome_creux_scalaire (p_polyf_creux_t p, float alpha);
+p_polyf_creux_t addition_polynome_creux (p_polyf_creux_t p1, p_polyf_creux_t p2);
+p_polyf_creux_t multiplication_polynome_creux_scalaire (p_polyf_creux_t p, float alpha);
 
-// float eval_polynome_creux (p_polyf_creux_t p, float x);
+float eval_polynome_creux (p_polyf_creux_t p, float x);
 
-// p_polyf_creux_t multiplication_polynomes_creux (p_polyf_creux_t p1, p_polyf_creux_t p2);
-// p_polyf_creux_t puissance_polynome_creux (p_polyf_creux_t p, int n);
-// p_polyf_creux_t composition_polynome_creux (p_polyf_creux_t p, p_polyf_creux_t q);
+p_polyf_creux_t multiplication_polynomes_creux (p_polyf_creux_t p1, p_polyf_creux_t p2);
+p_polyf_creux_t puissance_polynome_creux (p_polyf_creux_t p, int n);
+p_polyf_creux_t composition_polynome_creux (p_polyf_creux_t p, p_polyf_creux_t q);
