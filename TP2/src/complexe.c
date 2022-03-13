2ec99ca13803b9dda98e2741ff01cba464d2582b
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-13 17:34:06 +0100

    Implémentation des fonctions mult_complexe sans fonctions de tests associés

--- TP2/src/complexe.c
+++ TP2/src/complexe.c
@@ -1,78 +1,69 @@
 #include "complexe.h"
 
-complexe_float_t add_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
+complexe_float_t add_complexe_float(const complexe_float_t c1, const complexe_float_t c2)
 {
-  complexe_float_t r ;
+  complexe_float_t r;
 
-  r.real = c1.real + c2.real ;
-  r.imaginary = c1.imaginary + c2.imaginary ;
-  
-  return r ;
+  r.real = c1.real + c2.real;
+  r.imaginary = c1.imaginary + c2.imaginary;
+
+  return r;
 }
 
-complexe_double_t add_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
+complexe_double_t add_complexe_double(const complexe_double_t c1, const complexe_double_t c2)
 {
-  complexe_double_t r ;
+  complexe_double_t r;
 
-  r.real = c1.real + c2.real ;
-  r.imaginary = c1.imaginary + c2.imaginary ;
-  
-  return r ;
+  r.real = c1.real + c2.real;
+  r.imaginary = c1.imaginary + c2.imaginary;
+
+  return r;
 }
 
-complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
+complexe_float_t mult_complexe_float(const complexe_float_t c1, const complexe_float_t c2)
 {
-  complexe_float_t r ;
+  complexe_float_t r;
 
-  /* 
+  r.real = c1.real * c2.real - c1.imaginary * c2.imaginary; // i*i = i^2 = -1 so Re(r) = Mult(RE()) - Mult(Im())
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
+
+  return r;
+}
+
+complexe_double_t mult_complexe_double(const complexe_double_t c1, const complexe_double_t c2)
+{
+  complexe_double_t r;
+
+  r.real = c1.real * c2.real - c1.imaginary * c2.imaginary;
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
+
+  return r;
+}
+
+complexe_float_t div_complexe_float(const complexe_float_t c1, const complexe_float_t c2)
+{
+  complexe_float_t r;
+
+  /*
      a implementer
   */
 
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
-  
-  return r ;
+  r.real = 0.0;
+  r.imaginary = 0.0;
+
+  return r;
 }
 
-complexe_double_t mult_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
-  {
-  complexe_double_t r ;
-
-  /* 
-     a implementer
-  */
-  
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
-  
-  return r ;
-}
-  
-
-complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
+complexe_double_t div_complexe_double(const complexe_double_t c1, const complexe_double_t c2)
 {
-  complexe_float_t r ;
+  complexe_double_t r;
 
-  /* 
+  /*
      a implementer
   */
 
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
-  
-  return r ;
-}
+  r.real = 0.0;
+  r.imaginary = 0.0;
 
-complexe_double_t div_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
-{
-  complexe_double_t r ;
-
-  /* 
-     a implementer
-  */
-
-  r.real = 0.0 ;
-  r.imaginary = 0.0 ;
-  
-  return r ;
+  return r;
 }
