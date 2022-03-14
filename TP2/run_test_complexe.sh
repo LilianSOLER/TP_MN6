69b6a4ffcbdcaa84421f680f48ba28083b750f98
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-14 09:30:05 +0100

    Lancement des 4 test_complexe

--- TP2/run_test_complexe.sh
+++ TP2/run_test_complexe.sh
@@ -5,8 +5,14 @@ cd examples
 if [ -z "$1" ]
 then 
   ./test_complexe
+  ./test_complexe2
+  ./test_complexe3
+  ./test_complexe4
 else
   ./test_complexe > res/$1
+  ./test_complexe2 >> res/$1
+  ./test_complexe3 >> res/$1
+  ./test_complexe4 >> res/$1
 fi
 cd ..
 ./make_src_examples.sh clean
