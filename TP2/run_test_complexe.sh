da17449d6df74404a5fb9d73879251987be685db
SOLER Lilian <soler.lilian64@gmail.com>
2022-03-14 09:17:29 +0100

    Ajout de la redirection du résultat dans les fichiers du directory res

--- TP2/run_test_complexe.sh
+++ TP2/run_test_complexe.sh
@@ -2,6 +2,11 @@
 ./make_src_examples.sh clean
 ./make_src_examples.sh
 cd examples
-./test_complexe
+if [ -z "$1" ]
+then 
+  ./test_complexe
+else
+  ./test_complexe > res/$1
+fi
 cd ..
 ./make_src_examples.sh clean
