0ae8542d5dfda1301b93875f8206cf86f32b56be
bracquib <92532050+bracquib@users.noreply.github.com>
2022-03-23 17:28:19 +0100

    Add files via upload

--- TPBLAS/src/complexe.c
+++ TPBLAS/src/complexe.c
@@ -22,57 +22,42 @@ complexe_double_t add_complexe_double (const complexe_double_t c1, const complex
 
 complexe_float_t mult_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
 {
-  complexe_float_t r ;
+   complexe_float_t r;
 
-  /* 
-     a implementer
-  */
+  r.real = c1.real * c2.real - c1.imaginary * c2.imaginary; // i*i = i^2 = -1 so Re(r) = Mult(RE()) - Mult(Im())
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
 
-  r.real = c1.real*c2.real+c1.imaginary*c2.imaginary ;
-  r.imaginary = c1.real*c2.imaginary+c1.imaginary*c2.real ;
-  
   return r ;
 }
 
-complexe_double_t mult_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
-  {
-  complexe_double_t r ;
-
-  /* 
-     a implementer
-  */
-  
-  r.real = c1.real*c2.real+c1.imaginary*c2.imaginary ;
-  r.imaginary = c1.real*c2.imaginary+c1.imaginary*c2.real ;
-  
-  return r ;
-}
-  
-
-complexe_float_t div_complexe_float (const complexe_float_t c1, const complexe_float_t c2)
+complexe_double_t mult_complexe_double(const complexe_double_t c1, const complexe_double_t c2)
 {
-  complexe_float_t r ;
+  complexe_double_t r;
 
-  /* 
-     a implementer
-  */
+  r.real = c1.real * c2.real - c1.imaginary * c2.imaginary; // i*i = i^2 = -1 so Re(r) = Mult(RE()) - Mult(Im())
+  r.imaginary = c1.real * c2.imaginary + c1.imaginary * c2.real;
 
-  r.real = (c1.real*c2.real+c1.imaginary*c2.imaginary)/(c2.real*c2.real+c2.imaginary*c2.imaginary) ;
-  r.imaginary = ((-c1.real)*c2.imaginary+c1.imaginary*c2.real)/(c2.real*c2.real+c2.imaginary*c2.imaginary);
-  
-  return r ;
+  return r;
 }
 
-complexe_double_t div_complexe_double (const complexe_double_t c1, const complexe_double_t c2)
+complexe_float_t div_complexe_float(const complexe_float_t c1, const complexe_float_t c2)
 {
-  complexe_double_t r ;
+  complexe_float_t r;
 
-  /* 
-     a implementer
-  */
+  float denum = c1.real * c1.real + c2.real * c2.real;
+  r.real = (c1.real * c2.real + c1.imaginary * c2.imaginary) / denum;
+  r.imaginary = (c1.imaginary * c2.real + c1.real * c2.imaginary) / denum;
 
-  r.real = (c1.real*c2.real+c1.imaginary*c2.imaginary)/(c2.real*c2.real+c2.imaginary*c2.imaginary) ;
-  r.imaginary = ((-c1.real)*c2.imaginary+c1.imaginary*c2.real)/(c2.real*c2.real+c2.imaginary*c2.imaginary);
-  
-  return r ;
+  return r;
+}
+
+complexe_double_t div_complexe_double(const complexe_double_t c1, const complexe_double_t c2)
+{
+  complexe_double_t r;
+
+  double denum = c1.real * c1.real + c2.real * c2.real;
+  r.real = (c1.real * c2.real + c1.imaginary * c2.imaginary) / denum;
+  r.imaginary = (c1.imaginary * c2.real + c1.real * c2.imaginary) / denum;
+
+  return r;
 }
