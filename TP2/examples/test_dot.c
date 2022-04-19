cc869cf38b162617a5a09d4ff279cc31b0ea47f3
SOLER Lilian <soler.lilian64@gmail.com>
2022-04-19 17:50:31 +0200

    Factorisation de la fonction de test et perf - Test copy double - Fonction perf donne 0 en perf alors que le temps est bon

--- TP2/examples/test_dot.c
+++ TP2/examples/test_dot.c
@@ -108,271 +108,202 @@ void vector_print_comp_double(vcomp_double V)
   return;
 }
 
-void basic_main()
+void test(int type, int fonction)
 {
-  struct timeval start, end;
-  unsigned long long int start_tsc, end_tsc;
-
-  float res;
-  int i;
-
-  init_flop_tsc();
-
-  for (i = 0; i < NB_FOIS; i++)
-  {
-    vector_init(vec1, 1.0);
-    vector_init(vec2, 2.0);
-    res = 0.0;
-
-    start_tsc = _rdtsc();
-    res = mncblas_sdot(VECSIZE, vec1, 1, vec2, 1);
-    end_tsc = _rdtsc();
-
-    calcul_flop_tsc("sdot nano ", 2 * VECSIZE, end_tsc - start_tsc);
-  }
-
-  printf("res = %f\n", res);
-  printf("==========================================================\n");
-
-  init_flop_micro();
-
-  for (i = 0; i < NB_FOIS; i++)
-  {
-    vector_init(vec1, 1.0);
-    vector_init(vec2, 2.0);
-    res = 0.0;
-
-    TOP_MICRO(start);
-    res = mncblas_sdot(VECSIZE, vec1, 1, vec2, 1);
-    TOP_MICRO(end);
-
-    calcul_flop_micro("sdot micro", 2 * VECSIZE, tdiff_micro(&start, &end));
-  }
-
-  printf("res = %f\n", res);
-  printf("==========================================================\n");
-
-  init_flop_tsc();
-
-  for (i = 0; i < NB_FOIS; i++)
-  {
-    vector_init(vec1, 1.0);
-    vector_init(vec2, 2.0);
-    res = 0.0;
-
-    start_tsc = _rdtsc();
-    res = mncblas_sdot(VECSIZE, vec1, 1, vec2, 1);
-    end_tsc = _rdtsc();
-
-    calcul_flop_nano("sdot nano ", 2 * VECSIZE, end_tsc - start_tsc);
-  }
-
-  printf("res = %f\n", res);
-  printf("==========================================================\n");
-}
-
-void test_float(int fonction)
-{
-  printf("Test float\n");
-
+  printf("Test\n");
   srand(time(NULL));
   for (int i = 0; i < NB_FOIS; i++)
   {
-    vector_init(vec1, 1.0);
-    vector_init(vec2, 2.0);
-    vector_print(vec1);
-    vector_print(vec2);
-    printf("\n");
-    switch(fonction)
+    switch (type)
     {
     case 1:
-      mncblas_sswap(VECSIZE, vec1, 1, vec2, 1);
+      vector_init(vec1, 1.0);
+      vector_init(vec2, 2.0);
+      vector_print(vec1);
+      vector_print(vec2);
+      switch (fonction)
+      {
+      case 1:
+        mncblas_sswap(VECSIZE, vec1, 1, vec2, 1);
+        break;
+      case 2:
+        mncblas_scopy(VECSIZE, vec1, 1, vec2, 1);
+        break;
+      }
+      vector_print(vec1);
+      vector_print(vec2);
       break;
     case 2:
-      mncblas_scopy(VECSIZE, vec1, 1, vec2, 1);
+      vector_init_double_random(vec1d);
+      vector_init_double_random(vec2d);
+      vector_print_double(vec1d);
+      vector_print_double(vec2d);
+      switch (fonction)
+      {
+      case 1:
+        mncblas_dswap(VECSIZE, vec1d, 1, vec2d, 1);
+        break;
+      case 2:
+        mncblas_dcopy(VECSIZE, vec1d, 1, vec2d, 1);
+        break;
+      }
+      vector_print_double(vec1d);
+      vector_print_double(vec2d);
       break;
-    default:
-      printf("Erreur fonction\n");
+    case 3:
+      vector_init_comp_float_random(vec1cf);
+      vector_init_comp_float_random(vec2cf);
+      switch (fonction)
+      {
+      case 1:
+        mncblas_cswap(VECSIZE, vec1cf, 1, vec2cf, 1);
+        break;
+      case 2:
+        // mncblas_ccopy(VECSIZE, vec1cf, 1, vec2cf, 1);
+        break;
+      }
+      vector_print_comp_float(vec1cf);
+      vector_print_comp_float(vec2cf);
+      break;
+    case 4:
+      vector_init_comp_double_random(vec1cd);
+      vector_init_comp_double_random(vec2cd);
+      switch (fonction)
+      {
+      case 1:
+        mncblas_zswap(VECSIZE, vec1cd, 1, vec2cd, 1);
+        break;
+      case 2:
+        // mncblas_zcopy(VECSIZE, vec1cd, 1, vec2cd, 1);
+        break;
+      }
+      vector_print_comp_double(vec1cd);
+      vector_print_comp_double(vec2cd);
       break;
     }
-    vector_print(vec1);
-    vector_print(vec2);
     printf("\n");
   }
   printf("==========================================================\n");
 }
 
-void perf_float(int fonction)
+void perf(int type, int fonction)
 {
   unsigned long long int start_tsc, end_tsc;
 
-  init_flop_tsc();
+  init_flop_nano();
 
   for (int i = 0; i < NB_FOIS; i++)
   {
-    vector_init(vec1, 1.0);
-    vector_init(vec2, 2.0);
-    start_tsc = _rdtsc();
-    switch(fonction)
+    switch (type)
     {
     case 1:
-      mncblas_sswap(VECSIZE, vec1, 1, vec2, 1);
+      vector_init(vec1, 1.0);
+      vector_init(vec2, 2.0);
       break;
     case 2:
-      mncblas_scopy(VECSIZE, vec1, 1, vec2, 1);
+      vector_init_double_random(vec1d);
+      vector_init_double_random(vec2d);
       break;
-    default:
-      printf("Erreur fonction\n");
+    case 3:
+      vector_init_comp_float_random(vec1cf);
+      vector_init_comp_float_random(vec2cf);
+      break;
+    case 4:
+      vector_init_comp_double_random(vec1cd);
+      vector_init_comp_double_random(vec2cd);
       break;
     }
-    end_tsc = _rdtsc();
 
-    switch(fonction)
+    switch (type)
     {
     case 1:
-      calcul_flop_tsc("swaps nano", 2 * VECSIZE, end_tsc - start_tsc);
+      switch (fonction)
+      {
+      case 1:
+        start_tsc = _rdtsc();
+        mncblas_sswap(VECSIZE, vec1, 1, vec2, 1);
+        end_tsc = _rdtsc();
+        calcul_flop_nano("mncblas_sswap", 2 * VECSIZE, end_tsc - start_tsc);
+        break;
+      case 2:
+        start_tsc = _rdtsc();
+        mncblas_scopy(VECSIZE, vec1, 1, vec2, 1);
+        end_tsc = _rdtsc();
+        calcul_flop_nano("mncblas_scopy", 2 * VECSIZE, end_tsc - start_tsc);
+        break;
+      default:
+        printf("Error\n");
+        break;
+      }
       break;
     case 2:
-      calcul_flop_tsc("copys nano", 2 * VECSIZE, end_tsc - start_tsc);
+      switch (fonction)
+      {
+      case 1:
+        start_tsc = _rdtsc();
+        mncblas_dswap(VECSIZE, vec1d, 1, vec2d, 1);
+        end_tsc = _rdtsc();
+        calcul_flop_nano("mncblas_dswap", 2 * VECSIZE, end_tsc - start_tsc);
+        break;
+      case 2:
+        start_tsc = _rdtsc();
+        mncblas_dcopy(VECSIZE, vec1d, 1, vec2d, 1);
+        end_tsc = _rdtsc();
+        printf("%llu %llu\n", end_tsc, start_tsc);
+        printf("%llu\n", end_tsc - start_tsc);
+        calcul_flop_nano("mncblas_dcopy", 2 * VECSIZE, end_tsc - start_tsc);
+        break;
+      default:
+        printf("Error\n");
+        break;
+      }
       break;
-    default:
-      printf("Erreur fonction\n");
+    case 3:
+      switch (fonction)
+      {
+      case 1:
+        start_tsc = _rdtsc();
+        mncblas_cswap(VECSIZE, vec1cf, 1, vec2cf, 1);
+        end_tsc = _rdtsc();
+        calcul_flop_nano("mncblas_cswap", 2 * VECSIZE, end_tsc - start_tsc);
+        break;
+      case 2:
+        start_tsc = _rdtsc();
+        // mncblas_ccopy(VECSIZE, vec1cf, 1, vec2cf, 1);
+        end_tsc = _rdtsc();
+        // calcul_flop_nano("mncblas_ccopy", 2 * VECSIZE,  end_tsc - start_tsc);
+        break;
+      default:
+        printf("Error\n");
+        break;
+      }
+      break;
+    case 4:
+      switch (fonction)
+      {
+      case 1:
+        start_tsc = _rdtsc();
+        mncblas_zswap(VECSIZE, vec1cd, 1, vec2cd, 1);
+        end_tsc = _rdtsc();
+        calcul_flop_nano("mncblas_zswap", 2 * VECSIZE, end_tsc - start_tsc);
+        break;
+      case 2:
+        start_tsc = _rdtsc();
+        // mncblas_zcopy(VECSIZE, vec1cd, 1, vec2cd, 1);
+        end_tsc = _rdtsc();
+        // calcul_flop_nano("mncblas_zcopy", 2 * VECSIZE,  end_tsc - start_tsc);
+        break;
+      default:
+        printf("Error\n");
+        break;
+      }
       break;
     }
-    
-  }
-  printf("==========================================================\n");
-}
-
-void test_double()
-{
-  printf("Test swap  double\n");
-  //  printf("Test swap  double\n");
-
-  srand(time(NULL));
-  for (int i = 0; i < NB_FOIS; i++)
-  {
-    vector_init_double_random(vec1d);
-    vector_init_double_random(vec2d);
-    vector_print_double(vec1d);
-    vector_print_double(vec2d);
-    printf("\n");
-    mncblas_dswap(VECSIZE, vec1d, 2, vec2d, 2);
-    // mncblas_dcopy(VECSIZE, vec1d, 2, vec2d, 2)
-    vector_print_double(vec1d);
-    vector_print_double(vec2d);
-    printf("\n");
-  }
-  printf("==========================================================\n");
-}
-
-void perf_double()
-{
-  unsigned long long int start_tsc, end_tsc;
-
-  init_flop_tsc();
-
-  for (int i = 0; i < NB_FOIS; i++)
-  {
-    vector_init_double_random(vec1d);
-    vector_init_double_random(vec2d);
-
-    start_tsc = _rdtsc();
-    mncblas_dswap(VECSIZE, vec1d, 1, vec2d, 1);
-    // mncblas_dcopy(VECSIZE, vec1d, 2, vec2d, 2)
-    end_tsc = _rdtsc();
-
-    calcul_flop_tsc("swapd nano", 2 * VECSIZE, end_tsc - start_tsc);
-  }
-  printf("==========================================================\n");
-}
-void test_comp_float()
-{
-  printf("Test swap comp float\n");
-  //  printf("Test swap comp float\n");
-
-  srand(time(NULL));
-  for (int i = 0; i < NB_FOIS; i++)
-  {
-    vector_init_comp_float_random(vec1cf);
-    vector_init_comp_float_random(vec2cf);
-    vector_print_comp_float(vec1cf);
-    vector_print_comp_float(vec2cf);
-    printf("\n");
-    mncblas_cswap(VECSIZE, vec1cf, 2, vec2cf, 2);
-    // mncblas_ccopy(VECSIZE, vec1d, 2, vec2d, 2)
-    vector_print_comp_float(vec1cf);
-    vector_print_comp_float(vec2cf);
-    printf("\n");
-  }
-  printf("==========================================================\n");
-}
-void perf_comp_float()
-{
-  unsigned long long int start_tsc, end_tsc;
-
-  init_flop_tsc();
-
-  for (int i = 0; i < NB_FOIS; i++)
-  {
-    vector_init_comp_float_random(vec1cf);
-    vector_init_comp_float_random(vec2cf);
-
-    start_tsc = _rdtsc();
-    mncblas_cswap(VECSIZE, vec1cf, 1, vec2cf, 1);
-    // mncblas_ccopy(VECSIZE, vec1d, 2, vec2d, 2)
-
-    end_tsc = _rdtsc();
-
-    calcul_flop_tsc("swapcf nano", 2 * VECSIZE, end_tsc - start_tsc);
-  }
-  printf("==========================================================\n");
-}
-void test_comp_double()
-{
-  printf("Test swap comp double\n");
-  //  printf("Test swap comp double\n");
-
-  srand(time(NULL));
-  for (int i = 0; i < NB_FOIS; i++)
-  {
-    vector_init_comp_double_random(vec1cd);
-    vector_init_comp_double_random(vec2cd);
-    vector_print_comp_double(vec1cd);
-    vector_print_comp_double(vec2cd);
-    printf("\n");
-    mncblas_zswap(VECSIZE, vec1cd, 2, vec2cd, 2);
-    // mncblas_zcopy(VECSIZE, vec1d, 2, vec2d, 2)
-
-    vector_print_comp_double(vec1cd);
-    vector_print_comp_double(vec2cd);
-    printf("\n");
-  }
-  printf("==========================================================\n");
-}
-void perf_comp_double()
-{
-  unsigned long long int start_tsc, end_tsc;
-
-  init_flop_tsc();
-
-  for (int i = 0; i < NB_FOIS; i++)
-  {
-    vector_init_comp_double_random(vec1cd);
-    vector_init_comp_double_random(vec2cd);
-
-    start_tsc = _rdtsc();
-    mncblas_zswap(VECSIZE, vec1cd, 1, vec2cd, 1);
-    // mncblas_zcopy(VECSIZE, vec1d, 2, vec2d, 2)
-
-    end_tsc = _rdtsc();
-
-    calcul_flop_tsc("swapcd nano", 2 * VECSIZE, end_tsc - start_tsc);
   }
   printf("==========================================================\n");
 }
 
 int main(int argc, char **argv)
 {
-  perf_float(2);
+  perf(2, 1);
 }
--- TP2/src/copy.c
+++ TP2/src/copy.c
@@ -14,19 +14,19 @@ void mncblas_scopy(const int N, const float *X, const int incX,
   return ;
 }
 
-// void mncblas_dcopy(const int N, const double *X, const int incX, 
-//                  double *Y, const int incY)
-// {
-// register unsigned int i = 0 ;
-//   register unsigned int j = 0 ;
+void mncblas_dcopy(const int N, const double *X, const int incX, 
+                 double *Y, const int incY)
+{
+register unsigned int i = 0 ;
+  register unsigned int j = 0 ;
 
-//   for (; ((i < N) && (j < N)) ; i += incX, j += incY)
-//     {
-//       Y [j] = X [i] ;
-//     }
+  for (; ((i < N) && (j < N)) ; i += incX, j += incY)
+    {
+      Y [j] = X [i] ;
+    }
 
-//   return ;
-// }
+  return ;
+}
 
 // void mncblas_ccopy(const int N, const void *X, const int incX, 
 // 		                    void *Y, const int incY)
