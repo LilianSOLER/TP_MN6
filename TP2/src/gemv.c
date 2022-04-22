8a22d59d9824c0709c51e06754b47337b6057b81
Pierrick Catteau <pierrick.catteau@etu.univ-grenoble-alpes.fr>
2022-04-22 16:08:51 +0200

    ça c'est chaud

---
+++ TP2/src/gemv.c
@@ -0,0 +1,10 @@
+#include "mnblas.h"
+#include "complexe.h"
+
+void mncblas_sgemv(const MNCBLAS_LAYOUT layout,
+                 const MNCBLAS_TRANSPOSE TransA, const int M, const int N,
+                 const float alpha, const float *A, const int lda,
+                 const float *X, const int incX, const float beta,
+                 float *Y, const int incY){
+                     // A faire
+                 }
